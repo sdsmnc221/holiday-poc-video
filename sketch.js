3e579348c7318cef384eeb7673b27d1a8313f766
sdsmnc221 <antr.2201@gmail.com>
2022-09-07 17:28:41 +0200

    transition between two video on one plane

--- sketch.js
+++ sketch.js
@@ -3,162 +3,66 @@ import * as load from "load-asset";
 import { TWEEN } from "three/examples/jsm/libs/tween.module.min";
 import VID_0 from "./assets/xp-0.mp4";
 import VID_1 from "./assets/xp-1.mp4";
+import DISP_1 from "./assets/disp1.jpeg";
 
 let camera;
 
 let video0, video1;
-let text0, text1;
+let text0, text1, textures;
 let mesh0, mesh1;
+let current = 0;
 
 const vertexShader = `
-    uniform float uProgress;
-    uniform vec2 uMeshScale;
-    uniform vec2 uMeshPosition;
-    uniform vec2 uViewSize;
-
     varying vec2 vUv;
-
-    void main(){
-        vec3 pos = position.xyz;
-
-        float maxDistance = distance(vec2(0.),vec2(0.5));
-        float dist = distance(vec2(0.), uv-0.5);
-        float activation = smoothstep(0.,maxDistance,dist);
-		
-        float latestStart = 0.032;
-        float startAt = activation * latestStart;
-        float vertexProgress = smoothstep(startAt,1.,uProgress);
-     
-      // Scale to page view size/page size
-
-      vec2 scaleToViewSize = uViewSize / uMeshScale - 1.;
-      vec2 scale = vec2(
-        1. + scaleToViewSize * vertexProgress
-      );
-      pos.xy *= scale;
-      
-      // Move towards center 
-      pos.y += -uMeshPosition.y * vertexProgress;
-      pos.x += -uMeshPosition.x * vertexProgress;
-      
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(pos,1.);
+    
+    void main() {
         vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
     }
 `;
 
 const fragmentShader = `
-    uniform vec2 uvOffset;
-    uniform sampler2D uTexture;
-
+    uniform float time;
+    uniform float progress;
+    uniform float width;
+    uniform float scaleX;
+    uniform float scaleY;
+    uniform float transition;
+    uniform float radius;
+    uniform float swipe;
+    uniform sampler2D texture1;
+    uniform sampler2D texture2;
+    uniform sampler2D displacement;
+    uniform vec4 resolution;
     varying vec2 vUv;
+    varying vec4 vPosition;
 
-    void main()
-    {
-        gl_FragColor = texture2D(uTexture, vUv + uvOffset); 
+    float parabola( float x, float k ) {
+        return pow( 4. * x * ( 1. - x ), k );
+    }
+    
+    void main()	{
+        vec2 newUV = (vUv - vec2(0.5))*resolution.zw + vec2(0.5);
+        vec2 p = newUV;
+        vec2 start = vec2(0.5,0.5);
+        vec2 aspect = resolution.wz;
+        // vec2 uv = newUV;
+        vec2 uv = vUv;
+        float dt = parabola(progress, 1.);
+        vec4 noise = texture2D(displacement, fract(vUv+time*0.04));
+        float prog = progress*0.66 + noise.g * 0.04;
+        float circ = 1. - smoothstep(-width, 0.0, radius * distance(start*aspect, uv*aspect) - prog*(1.+width));
+        float intpl = pow(abs(circ), 1.);
+        vec4 t1 = texture2D( texture1, (uv - 0.5) * (1.0 - intpl) + 0.5 ) ;
+        vec4 t2 = texture2D( texture2, (uv - 0.5) * intpl + 0.5 );
+        gl_FragColor = mix( t1, t2, intpl );
+        // gl_FragColor = texture2D(texture1, vec2(vUv));
     }
 `;
 
-class GridToFullscreenEffect {
-  constructor(width, height, scene, camera, text) {
-    this.width = width;
-    this.height = height;
-    this.text = text;
-    this.camera = camera;
-    this.scene = scene;
-
-    this.uniforms = {
-      uProgress: new THREE.Uniform(0),
-      uMeshScale: new THREE.Uniform(new THREE.Vector2(0, 0)),
-      uMeshPosition: new THREE.Uniform(new THREE.Vector2(0, 0)),
-      uViewSize: new THREE.Uniform(new THREE.Vector2(1, 1)),
-      uTexture: { type: "t", value: this.text },
-      uvOffset: { type: "v", value: new THREE.Vector2(0, 0) },
-    };
-
-    const viewSize = this.getViewSize();
-    this.uniforms.uViewSize.value.x = viewSize.width;
-    this.uniforms.uViewSize.value.y = viewSize.height;
-    const segments = 128;
-    const geometry = new THREE.PlaneGeometry(1, 1, segments, segments);
-    const material = new THREE.ShaderMaterial({
-      side: THREE.DoubleSide,
-      uniforms: this.uniforms,
-      vertexShader: vertexShader,
-      fragmentShader: fragmentShader,
-    });
-    this.mesh = new THREE.Mesh(geometry, material);
-    this.scene.add(this.mesh);
-  }
-
-  getViewSize() {
-    const fovInRadians = (this.camera.fov * Math.PI) / 180;
-    const height = Math.abs(
-      this.camera.position.z * Math.tan(fovInRadians / 2) * 2
-    );
-
-    return { width: height * this.camera.aspect, height };
-  }
-
-  getRectSize() {
-    return {
-      width: 0.1,
-      height: 0.1,
-      left: this.width / 2 - 0.1 / 2,
-      top: this.height / 2 - 0.1 / 2,
-    };
-  }
-
-  onGridImageClick() {
-    // getBoundingClientRect gives pixel units relative to the top left of the pge
-    const rect = this.getRectSize();
-    const viewSize = this.getViewSize();
-
-    // 1. Transform pixel units to camera's view units
-    const widthViewUnit = (rect.width * viewSize.width) / this.width;
-    const heightViewUnit = (rect.height * viewSize.height) / this.height;
-    let xViewUnit = (rect.left * viewSize.width) / this.width;
-    let yViewUnit = (rect.top * viewSize.height) / this.height;
-
-    // 2. Make units relative to center instead of the top left.
-    xViewUnit = xViewUnit - viewSize.width / 2;
-    yViewUnit = yViewUnit - viewSize.height / 2;
-
-    // 3. Make the origin of the plane's position to be the center instead of top Left.
-    let x = xViewUnit + widthViewUnit / 2;
-    let y = -yViewUnit - heightViewUnit / 2;
-
-    // 4. Scale and position mesh
-    const mesh = this.mesh;
-    // Since the geometry's size is 1. The scale is equivalent to the size.
-    mesh.scale.x = widthViewUnit;
-    mesh.scale.y = heightViewUnit;
-    mesh.position.x = x;
-    mesh.position.y = y;
-
-    this.uniforms.uMeshPosition.value.x = x / widthViewUnit;
-    this.uniforms.uMeshPosition.value.y = y / heightViewUnit;
-    this.uniforms.uMeshScale.value.x = widthViewUnit;
-    this.uniforms.uMeshScale.value.y = heightViewUnit;
-
-    new TWEEN.Tween(this.uniforms.uProgress)
-      .to({ value: 1 }, 3200)
-      .easing(TWEEN.Easing.Sinusoidal.InOut)
-      .start();
-
-    // setTimeout(
-    //   () =>
-    //     new TWEEN.Tween(this.uniforms.uProgress)
-    //       .to({ value: 0 }, 3200)
-    //       .easing(TWEEN.Easing.Sinusoidal.InOut)
-    //       .start(),
-    //   6400
-    // );
-  }
-}
-
 export let init = async ({ scene, width, height }) => {
-  camera = new THREE.PerspectiveCamera(45, 16 / 9, 0.1, 100);
-  camera.position.z = 16;
+  camera = new THREE.PerspectiveCamera(45, 16 / 9, 0.1, 1000);
+  camera.position.z = 1000;
   camera.lookAt(new THREE.Vector3());
 
   video0 = await load(VID_0);
@@ -166,16 +70,52 @@ export let init = async ({ scene, width, height }) => {
 
   text0 = new THREE.VideoTexture(video0);
   text0.needsUpdate = true;
-  mesh0 = new THREE.Mesh(
-    new THREE.PlaneGeometry(width, height),
-    new THREE.MeshBasicMaterial({ map: text0 })
-  );
-  mesh0.scale.set(0.02, 0.02, 0.1);
-  scene.add(mesh0);
-
   text1 = new THREE.VideoTexture(video1);
   text1.needsUpdate = true;
-  mesh1 = new GridToFullscreenEffect(width, height, scene, camera, text1);
+  textures = [text0, text1];
+
+  //   text0 = new THREE.TextureLoader().load(IMG_0);
+  //   text0.needsUpdate = true;
+  //   text1 = new THREE.TextureLoader().load(IMG_1);
+  //   text1.needsUpdate = true;
+  //   textures = [text0, text1];
+
+  //   mesh0 = new THREE.Mesh(
+  //     new THREE.PlaneGeometry(width, height),
+  //     new THREE.MeshBasicMaterial({ map: text0 })
+  //   );
+  //   scene.add(mesh0);
+
+  const uniforms = {
+    time: { type: "f", value: 0 },
+    progress: { type: "f", value: 0 },
+    border: { type: "f", value: 0 },
+    intensity: { type: "f", value: 0 },
+    scaleX: { type: "f", value: 0 },
+    scaleY: { type: "f", value: 40 },
+    transition: { type: "f", value: 40 },
+    swipe: { type: "f", value: 0 },
+    texture1: { type: "f", value: text0 },
+    texture2: { type: "f", value: text1 },
+    displacement: {
+      type: "f",
+      value: new THREE.TextureLoader().load(DISP_1),
+    },
+    resolution: { type: "v4", value: new THREE.Vector4() },
+    radius: { value: 0.2, type: "f", min: 0.1, max: 2 },
+    width: { value: 0.5, type: "f", min: 0, max: 1 },
+  };
+
+  mesh1 = new THREE.Mesh(
+    new THREE.PlaneGeometry(width, height),
+    new THREE.ShaderMaterial({
+      side: THREE.DoubleSide,
+      uniforms,
+      vertexShader: vertexShader,
+      fragmentShader: fragmentShader,
+    })
+  );
+  scene.add(mesh1);
 
   const app = document.querySelector("#app");
   const btn = document.createElement("button");
@@ -184,7 +124,19 @@ export let init = async ({ scene, width, height }) => {
     height / 2.4
   }px; z-index: 999`;
   btn.addEventListener("click", () => {
-    mesh1.onGridImageClick();
+    let len = textures.length;
+    let nextTexture = textures[(current + 1) % len];
+    mesh1.material.uniforms.texture2.value = nextTexture;
+
+    new TWEEN.Tween(mesh1.material.uniforms.progress)
+      .to({ value: 1 }, 3200)
+      .easing(TWEEN.Easing.Cubic.Out)
+      .onComplete(() => {
+        current = (current + 1) % len;
+        mesh1.material.uniforms.texture1.value = nextTexture;
+        mesh1.material.uniforms.progress.value = 0;
+      })
+      .start();
   });
   app.appendChild(btn);
 
@@ -196,6 +148,7 @@ export let init = async ({ scene, width, height }) => {
 
 export let update = ({ renderer, scene, time, deltaTime }) => {
   TWEEN.update();
+  if (mesh1) mesh1.material.uniforms.time.value = time;
   renderer.render(scene, camera);
 };
 
